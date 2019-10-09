ef76013a5b08488a42fed6bcb33388fce1696b99
matheus s. silva antonio <viciogamer157@gmail.com>
2019-10-09 17:55:23 -0300

    criar alguns componentes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Navbar from './componentes/Navbar';
+import Home from './componentes/Home';
+import Cadastro from './componentes/Cadastro/Cadastro';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route path="/cadastro" component={Cadastro} />
+    </BrowserRouter>
+  )
 }
-
 export default App;
---
+++ src/componentes/Navbar.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+
+import "./Navbar.scss";
+
+const Navbar = () => {
+  const [menuAtivo, setMenuAtivo] = useState(false);
+
+  const abrirMenu = () => {
+    setMenuAtivo(!menuAtivo);
+  };
+
+  const fecharMenu = () => {
+    setMenuAtivo(false)
+  }
+
+  return (
+    <div className="Navbar">
+      <nav>
+        <div onClick={abrirMenu} id="menu-hamburguer">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </nav>
+      <section className={`menu ${menuAtivo ? "ativo" : ""}`}>
+        <Link onClick={fecharMenu} to="/">Home</Link>
+        <Link onClick={fecharMenu} to="/pagamento">Pagamento</Link>
+        <Link onClick={fecharMenu} to="/carrinho">Carrinho</Link>
+        <Link onClick={fecharMenu} to="/produtos">Produtos</Link>
+      </section>
+    </div>
+  );
+};
+export default Navbar;
